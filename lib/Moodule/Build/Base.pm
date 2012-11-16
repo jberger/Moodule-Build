76ef495fce47d96bffc3eab7a39676706914964e
Joel Berger <joel.a.berger@gmail.com>
2012-11-15 20:55:59 -0600

    move HashStore helper into HashStore class

--- lib/Moodule/Build/Base.pm
+++ lib/Moodule/Build/Base.pm
@@ -3,27 +3,7 @@ package Moodule::Build::Base;
 use Moo;
 use Carp;
 
-use Moodule::Build::HashStore;
-
-sub HashStore {
-  my $name = shift or croak "Must specify the name of the HashStore";
-  my $default = @_ ? shift : {};
-  if ($default && ref $default ne 'HASH') {
-    croak "HashStore default must be a hash reference";
-  }
-
-  return (
-    "_$name",
-    is => 'rw',
-    init_arg => $name,
-    handles => { $name => 'accessor' },
-    coerce => sub {
-      return $_[0] if eval{ $_[0]->isa('Moodule::Build::HashStore') };
-      return Moodule::Build::HashStore->new( data => $_[0] );
-    },
-    default => sub { Moodule::Build::HashStore->new( data => $default ) },
-  );
-}
+use Moodule::Build::HashStore qw/HashStore/;
 
 has 'dist_name' => (
   is => 'ro',
--- lib/Moodule/Build/HashStore.pm
+++ lib/Moodule/Build/HashStore.pm
@@ -1,6 +1,10 @@
 package Moodule::Build::HashStore;
 
 use Moo;
+use Carp;
+use Exporter 'import';
+
+our @EXPORT_OK = qw/HashStore/;
 
 has 'data' => (
   is => 'ro',
@@ -19,6 +23,26 @@ sub accessor {
     $data->{$key} = $value;
   }
   return $data->{$key};
+}
+
+sub HashStore {
+  my $name = shift or croak "Must specify the name of the HashStore";
+  my $default = @_ ? shift : {};
+  if ($default && ref $default ne 'HASH') {
+    croak "HashStore default must be a hash reference";
+  }
+
+  return (
+    "_$name",
+    is => 'rw',
+    init_arg => $name,
+    handles => { $name => 'accessor' },
+    coerce => sub {
+      return $_[0] if eval{ $_[0]->isa('Moodule::Build::HashStore') };
+      return Moodule::Build::HashStore->new( data => $_[0] );
+    },
+    default => sub { Moodule::Build::HashStore->new( data => $default ) },
+  );
 }
 
 1;
